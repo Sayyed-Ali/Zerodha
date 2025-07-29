99308a81ecf9345e1ed35e4689680c86eee6cd44
Sayyad Ali <qadriali54@gmail.com>
2025-07-29 15:14:58 +0530

    designed complete dashboard with working buttons

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import React from 'react'
 
 function App() {
-
   return (
-    <>
-    </>
+    <> <Home /></>
+
   )
 }
 
